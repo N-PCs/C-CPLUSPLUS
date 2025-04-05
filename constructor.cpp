e7d20f1f6779b92f7492ac858359213731396168
Neel Pandey <neelpandeyofficial@gmail.com>
2025-04-05 23:05:16 +0530

    Add files via upload

---
+++ constructor.cpp
@@ -0,0 +1,9 @@
+/* Constructor - special method/function invoked
+                 automatically at time of object creation.*/
+/* Properties - 
+              - same name as class
+              - doesn't have return type
+              - called automatically
+              - memory allocation happens when it is called*/
+
+              
