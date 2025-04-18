68437600d5c4c7bb8f85cd82e9eccc1e1c66dd8c
Neel Pandey <neelpandeyofficial@gmail.com>
2025-04-18 23:42:29 +0530

    Update practical_q5.cpp

--- practice_programs/practical_q5.cpp
+++ practice_programs/practical_q5.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 inline int calcArea(int side) {
-    return side * side;
+    return side*side;
 }
 int main() {
-    int length;
+    int len;
     cout << "Enter the side length of the square: ";
-    cin >> length;
-    int area = calcArea(length);
+    cin >> len;
+    int area = calcArea(len);
     cout << "Area of the square is: " << area << endl;
     return 0;
 }
