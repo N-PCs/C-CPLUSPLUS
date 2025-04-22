5110e92be3a89ec2bf7d622b7beb13d8618c0b86
Neel Pandey <neelpandeyofficial@gmail.com>
2025-04-22 17:30:27 +0530

    Add files via upload

---
+++ inheritance/single_inheritance.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// Base class
+class Animal {
+public:
+    void eat() {
+        cout << "Animal is eating..." << endl;
+    }
+};
+
+// Derived class (single inheritance)
+class Dog : public Animal {
+public:
+    void bark() {
+        cout << "Dog is barking..." << endl;
+    }
+};
+
+int main() {
+    Dog myDog;
+    myDog.eat();  // Inherited from Animal
+    myDog.bark(); // Own method
+    return 0;
+}
