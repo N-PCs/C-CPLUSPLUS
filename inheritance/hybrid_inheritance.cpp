49dd2b5b5495a4005542da82435975d52d74628d
Neel Pandey <neelpandeyofficial@gmail.com>
2025-04-22 17:32:22 +0530

    Add files via upload

---
+++ inheritance/hybrid_inheritance.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+// Base class 1
+class Person {
+public:
+    void displayPerson() {
+        cout << "I am a person" << endl;
+    }
+};
+
+// Base class 2
+class Employee {
+public:
+    void displayEmployee() {
+        cout << "I am an employee" << endl;
+    }
+};
+
+// Derived class 1 (hierarchical)
+class Student : public Person {
+public:
+    void displayStudent() {
+        cout << "I am a student" << endl;
+    }
+};
+
+// Derived class 2 (multiple inheritance)
+class WorkingStudent : public Person, public Employee {
+public:
+    void displayWorkingStudent() {
+        cout << "I am a working student" << endl;
+    }
+};
+
+int main() {
+    Student s;
+    WorkingStudent ws;
+    
+    s.displayPerson();
+    s.displayStudent();
+    
+    ws.displayPerson();
+    ws.displayEmployee();
+    ws.displayWorkingStudent();
+    
+    return 0;
+}
