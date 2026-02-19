702a315487ec820dcf725389b3da09e5c1e87665
Neel Pandey <neelpandeyofficial@gmail.com>
2026-02-19 21:13:12 +0530

    Create bubble_sort.c

---
+++ C/bubble_sort.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    int swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = 0; // Optimization: check if any swap happened
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap adjacent elements
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+        // If no two elements were swapped in the inner loop, break
+        if (swapped == 0) break;
+    }
+}
