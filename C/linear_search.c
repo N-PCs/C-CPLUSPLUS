a504f94c4cbdfe528db22a2c6809713337d50679
Neel Pandey <neelpandeyofficial@gmail.com>
2026-02-20 22:46:04 +0530

    Add linear search function
    Implement linear search algorithm in C.

---
+++ C/linear_search.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Returns the index if found, otherwise returns -1
+int linearSearch(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
