bb39a47ce816126246fab9d396fd77f3b29b7799
Neel Pandey <neelpandeyofficial@gmail.com>
2026-05-22 22:57:07 +0530

    Add pattern2.cpp to print asterisks triangle
    This program prints a right-angled triangle pattern of asterisks based on user input.

---
+++ DSA/Patterns /pattern2.cpp
@@ -0,0 +1,23 @@
+/*  Pattern 2
+            *
+            **
+            ***
+            ****
+            *****
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<n+1;i++){
+        cout<<"*";
+        for(int j=1;j<i;j++){
+            cout<<"*";
+        }
+        cout<<'\n';
+    }
+    return 0;
+}
